d69f49290899dd5c1f0efd3466597092b9ae2d8f
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-11-22 23:34:53 +0300

    4th ex

---
+++ dzmine/dz1.4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+var i = 0;
+if (n < 2)
+{
+Console.Write("0");
+}
+else if (n % 2 == 0)
+{while (i < n )
+    {
+    i += 2;
+    Console.Write($"{i}");
+    Console.Write(" ");
+    }
+}
+else 
+{while (i < (n - 1) )
+    {
+    i += 2;
+    Console.Write($"{i}");
+    Console.Write(" ");
+    }
+}
